51a4932b1bb6391d6acc52d23dcd5797fb7f5cd5
Carol Hanna <carol.z.hanna@gmail.com>
2022-06-24 15:54:11 +0100

    Fixing ZuneBug compilation script

--- tests/ZuneBug/prepareZuneBug.sh
+++ tests/ZuneBug/prepareZuneBug.sh
@@ -8,19 +8,19 @@
 # Does the compile script build the test files?
 
 PATHTOGENPROG="$1"
-JUNITJARS="$2"
+JUNITJARS="$PATHTOGENPROG"/lib
 
 PATHTOZUNEBUG=`pwd`
 
-if [[ ! -d bin/ ]] ; then
+if [ ! -d bin/ ] ; then
     mkdir bin
 fi
 
 javac -d bin/ src/packageZuneBug/ZuneBug.java 
 javac -classpath $JUNITJARS/junit-4.12.jar:$JUNITJARS/hamcrest-core-1.3.jar:bin/ -sourcepath src/tests/*java -d bin/ src/tests/*java
-rm -rf bin/packageZuneBug/
+#rm -rf bin/packageZuneBug/
 
-PACKAGEDIR=${JAVADIR//"/"/"."}
+#PACKAGEDIR=${JAVADIR//"/"/"."}
 
 #Create config file 
 FILE=./zuneBug.config
@@ -31,7 +31,7 @@ seed = 0
 classTestFolder = bin/
 workingDir = $PATHTOZUNEBUG/
 outputDir = $PATHTOZUNEBUG/tmp/
-libs = $PATHTOGENPROG/lib/junit-4.10.jar:$PATHTOGENPROG/lib/junittestrunner.jar
+libs = $PATHTOGENPROG/lib/junit-4.12.jar:$PATHTOGENPROG/lib/junittestrunner.jar:$JUNITJARS/hamcrest-core-1.3.jar:$PATHTOZUNEBUG/bin/
 sanity = yes
 regenPaths
 sourceDir = src/
