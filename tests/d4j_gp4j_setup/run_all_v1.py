3c7f36cdb5764b8bdd299bce4ec69f5354f626d4
carolhanna01 <carol.z.hanna@gmail.com>
2022-07-03 17:54:03 +0100

    Adding run all defects + run successful jarfly defects scripts

---
+++ tests/d4j_gp4j_setup/run_all_v1.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+import sys
+import argparse
+
+if __name__ == '__main__':
+    
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('javaVM')
+    args = parser.parse_args()
+
+    args = parser.parse_args()
+    for filename in os.listdir("./d4j_v1.1.0_active_bugs"):
+        f = open("./d4j_v1.1.0_active_bugs/" + filename, "r")
+        for line in f.readlines():
+            bug = line.split(",")[0].strip()
+            print(f"./prepare_bug.sh {filename} {bug} humanMade 1 ExamplesCheckedOut {args.javaVM} {args.javaVM}  \"false\" ./ \"false\" ./")
+            os.system(f"./prepare_bug.sh {filename} {bug} humanMade 1 ExamplesCheckedOut {args.javaVM} {args.javaVM}  \"false\" ./ \"false\" ./")
+            os.system(f"./repair_bug.sh {filename} {bug} allHuman 1 ExamplesCheckedOut 1 20 false {args.javaVM} {args.javaVM}  false \"\" false \"\"")
+          
+            
+
+
+
+            
+
+
+
+
---
+++ tests/d4j_gp4j_setup/run_patched_v1.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+import sys
+import argparse
+
+if __name__ == '__main__':
+    
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('javaVM')
+    args = parser.parse_args()
+
+    args = parser.parse_args()
+
+    f = open("./d4j_v1.1.0_patched_bugs", "r")
+    for line in f.readlines():
+        project = line.split(" ")[0].strip()
+        bug = line.split(" ")[1].strip()
+        print(f"./prepare_bug.sh {project} {bug} humanMade 1 ExamplesCheckedOut {args.javaVM} {args.javaVM}  \"false\" ./ \"false\" ./")
+        os.system(f"./prepare_bug.sh {project} {bug} humanMade 1 ExamplesCheckedOut {args.javaVM} {args.javaVM}  \"false\" ./ \"false\" ./")
+        os.system(f"./repair_bug.sh {project} {bug} allHuman 1 ExamplesCheckedOut 1 20 false {args.javaVM} {args.javaVM}  false \"\" false \"\"")
